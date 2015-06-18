7b0f0a9aa42a4352e73cc0e4369e0fcaa7cc0a58
sascanagl <carl.nagle@sas.com>
2015-06-18 20:40:27 +0100

    Revert "Initial GitHub snapshot commit of SAFS Robotium Remote Control Messenger"
    This reverts commit 9764de758026bc69774cef1aa85d2ee0b17e0984.

--- src/org/safs/android/messenger/MultipleParcelListener.java
+++
@@ -1,7 +0,0 @@
-package org.safs.android.messenger;
-
-public interface MultipleParcelListener {
-
-	public void onAllParcelsHaveBeenHandled(String messageID);
-	public void onParcelHasBeenHandled(String messageID, int index);
-}
