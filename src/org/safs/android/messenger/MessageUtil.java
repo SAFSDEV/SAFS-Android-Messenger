4dde907a5869ac389cc9a4ec2abeae1b1d8eaa74
sbjlwa <lei.wang@sas.com>
2017-07-14 10:49:10 +0800

    Pushed latest code from branch 'open_source_version' of gitlab.

--- src/org/safs/android/messenger/MessageUtil.java
+++ src/org/safs/android/messenger/MessageUtil.java
@@ -1,7 +1,20 @@
-/** 
- ** Copyright (C) SAS Institute, All rights reserved.
- ** General Public License: http://www.opensource.org/licenses/gpl-license.php
- **/
+/**
+ * Copyright (C) SAS Institute, All rights reserved.
+ * General Public License: https://www.gnu.org/licenses/gpl-3.0.en.html
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+**/
 package org.safs.android.messenger;
 
 import android.os.Bundle;
@@ -11,7 +24,7 @@ import android.os.Parcelable;
 /**
  * 
  * @author Carl Nagle, SAS Institute, Inc.
- * @since   FEB 04, 2012	(Carl Nagle)	Initial version
+ * @since   FEB 04, 2012	(CarlNagle)	Initial version
  *   <br>	APR 25, 2013	(LeiWang)	Add some methods to assist handling message of big size.
  */
 public class MessageUtil extends org.safs.sockets.Message{
